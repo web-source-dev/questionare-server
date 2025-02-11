e3bbfaff4937e2834c261d30a64aca3bf449db3e
web-source-dev <muhammadnouman72321@gmail.com>
2025-02-11 11:28:29 +0500

    index

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-const corsOptions = {
-  origin: '*', // Allow all origins
-  optionsSuccessStatus: 200
-};
-
 app.use(cors(corsOptions)); // Use cors middleware with options
 
 const corsOptions = {
